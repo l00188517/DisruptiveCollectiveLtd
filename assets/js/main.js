80861b7ce53a89f68a07c1e51267e824eabdf1c2
Mathieu Bizumuremyi <l00188517@atu.ie>
2025-11-11 20:51:19 +0000

    Added Appointment selector and Removed career.

--- assets/js/main.js
+++ assets/js/main.js
@@ -216,4 +216,39 @@
   window.addEventListener('load', navmenuScrollspy);
   document.addEventListener('scroll', navmenuScrollspy);
 
+
+
+
+  /**
+   * Modal Popup
+   */
+
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btns = document.getElementsByClassName("myBtn");
+
+  for (var i = 0; i < btns.length; i++) {
+    // When the user clicks the button, open the modal
+    btns[i].onclick = function() {
+      modal.style.display = "block";
+    }
+  }
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0]; 
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  
 })();
